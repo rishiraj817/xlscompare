0326577e72e4e97f0ba09a355bb76bf6dc49ddcb
Rishiraj <Rishiraj@192.168.1.8>
2021-03-09 05:45:23 -0800

    first copy

---
+++ src/mainDesign/Password.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainDesign;
+
+/**
+ *
+ * @author Hp
+ */
+public class Password {
+    
+    public int chechk(String first,String second)
+    {
+    if(first.compareTo(second)==0)
+    {
+   return 1;
+    }
+   else
+        return 0;
+    
+}
+     
+}
